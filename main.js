2e26b29e085e4288970c51667b6692bd73ae0db9
Kevin Abreu <kevinrabreu@hotmail.com>
2019-05-20 14:09:24 -0400

    I finally think I got it. Thank god

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 class Classic{
   constructor(art_url){
     this.frame = 'black-border'
-    this.art_url = 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg'
+    this.art_url = art_url
   }
   render(){
     let thing = document.createElement('div')
     thing.style.width = '1000px'
     thing.style.height = '1000px'
     thing.style.backgroundColor = 'red'
-    thing.style.background = Classic.art_url
+    thing.style.backgroundImage = `url(${this.art_url})`
     document.body.appendChild(thing)
     // this.art_url = ''
   }
 }
+
+let pic = new Classic("https://i.ytimg.com/vi/kJ2dr9jAThY/maxresdefault.jpg")
+let pic2 = new Classic("https://i.redd.it/daoyaisk14k11.jpg")
+// console.log(pic.rend)
+pic.render()
+pic2.render()
